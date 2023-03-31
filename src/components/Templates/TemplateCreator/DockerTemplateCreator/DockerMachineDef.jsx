a1702a9fbc7f4b46a8eed4e343bf4240dea29b67
DvEyZ <szymonkwi@outlook.com>
2023-03-31 12:31:19 +0200

    fix: wrapped more clickable elements in buttons

--- src/components/Templates/TemplateCreator/DockerTemplateCreator/DockerMachineDef.jsx
+++ src/components/Templates/TemplateCreator/DockerTemplateCreator/DockerMachineDef.jsx
@@ -28,17 +28,21 @@ export class DockerMachineDef extends React.Component
                 <div className='header'>
                     <h3>{this.props.name}</h3>
                     <div style={{margin:'auto'}}></div>
-                    <img src={`/img/icons/arrow-${this.state.expanded ? 'down' : 'right'}.svg`} alt={`${this.state.expanded ? 'collapse' : 'expand'}`}
-                    className='expander' onClick={() => {this.setState({expanded: !this.state.expanded})}}/>
+                    <button className='pod-element-button' onClick={() => {this.setState({expanded: !this.state.expanded})}}>
+                        <img src={`/img/icons/arrow-${this.state.expanded ? 'down' : 'right'}.svg`} alt={`${this.state.expanded ? 'collapse' : 'expand'}`}
+                        className='expander'/>
+                    </button>
                 </div>
                 <div className={`body ${!this.state.expanded && 'collapsed'}`}>
                     <hr/>
                     <div className='hc'>
                         <h4>Port redirections</h4>
                         <div style={{margin:'auto'}}></div>
-                        <img src='/img/icons/plus.svg' alt='create' onClick={() => {
-                            this.setState({ports: [...this.state.ports, {id:this.state.i}], i: this.state.i+1, });
-                        }}/>
+                        <button className='pod-element-button' onClick={() => {
+                                this.setState({ports: [...this.state.ports, {id:this.state.i}], i: this.state.i+1, });
+                            }}>
+                            <img src='/img/icons/plus.svg' alt='create'/>
+                        </button>
                     </div>
                     <div className='port-redirections'>
                         {
