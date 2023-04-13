a5fbea8d33358b8b9c13d6ce28b5451bd05dfb40
DvEyZ <szymonkwi@outlook.com>
2023-04-13 08:19:44 +0200

    chore: added version info in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import packageJson from '../../../package.json';
+
 import './Footer.css';
 
 export class Footer extends React.Component
@@ -8,6 +10,7 @@ export class Footer extends React.Component
         return(
             <div id='footer'>
                 <div>Made with 💙 by <a className='a-link' href='https://github.com/DvEyZ'>Szymon Kwiręg</a></div>
+                <div>Version {packageJson.version}</div>
             </div>
         )
     }
